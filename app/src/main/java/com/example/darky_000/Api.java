16d42d0b9ec1218094f48c93ccadc1d9d751e0da
William Fujimoto <fujimtsan@gmail.com>
2016-12-09 14:43:37 -0800

    Api Key

---
+++ app/src/main/java/com/example/darky_000/Api.java
@@ -0,0 +1,9 @@
+package com.example.darky_000;
+
+/**
+ * Created by darky_000 on 12/9/2016.
+ */
+
+public final class Api {
+    public static final String apikey = "783e741d1d3316397c7e3b2b175025b";
+}

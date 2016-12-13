92ca87067db81ffe0153f1c312422586f23c86d2
jchristiansen55 <jchrist1@mail.sfsu.edu>
2016-12-13 12:53:47 -0800

    Takes user to turn location and wifi on

--- app/src/main/java/com/example/darky_000/SingleFragmentActivity.java
+++ app/src/main/java/com/example/darky_000/SingleFragmentActivity.java
@@ -1,13 +1,17 @@
 package com.example.darky_000;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.example.darky_000.R;
@@ -19,6 +23,7 @@ import com.example.darky_000.R;
 public abstract class SingleFragmentActivity extends FragmentActivity {
 
     protected abstract Fragment createFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +43,49 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
         NetworkInfo nInfo = cManager.getActiveNetworkInfo();
         if(nInfo!=null && nInfo.isConnected()){
             Toast.makeText(this, "Network is available", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this,"Network is not available", Toast.LENGTH_LONG).show();
+        }else {
+            // Toast.makeText(this, "Network is not available", Toast.LENGTH_LONG).show();
+            final AlertDialog.Builder buildObj2 = new AlertDialog.Builder(this);
+            buildObj2.setMessage("Network is disabled, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            startActivity(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog message = buildObj2.create();
+            message.show();
         }
+
+
+
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
             Toast.makeText(this, "Location is enabled", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this, "Location is disabled please turn it on!", Toast.LENGTH_LONG).show();
+            final AlertDialog.Builder buildObj = new AlertDialog.Builder(this);
+            buildObj.setMessage("Location disabled, do you want to enable it?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog message = buildObj.create();
+            message.show();
         }
     }
-
 }
+
+
+
